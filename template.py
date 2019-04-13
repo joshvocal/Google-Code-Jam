ce58d43347b647ebab8c5a340d50cc639b900918
Josh Vocal <joshvocal@gmail.com>
2019-04-13 09:19:21 -0700

    Add template

---
+++ template.py
@@ -0,0 +1,11 @@
+# Solution
+def solve(t, N): 
+    print('Case #{}: {}'.format(t, "Hello World"))
+
+
+# Inputs
+T = int(input())
+for t in range(1, T + 1):
+    N = int(input())
+
+    solve(t, N)
